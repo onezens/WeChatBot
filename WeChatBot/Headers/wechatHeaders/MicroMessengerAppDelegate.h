d571981a12c0f5f71f8d9866f7a17fcd1eb62191
wz <mail@onezen.cc>
2019-03-28 16:22:05 +0800

    init project config

---
+++ WeChatBot/Headers/wechatHeaders/MicroMessengerAppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  MicroMessengerAppDelegate.h
+//  WeChatBot
+//
+//  Created by wz on 2019/3/28.
+//  Copyright © 2019 wz. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface MicroMessengerAppDelegate : NSObject <UIApplicationDelegate>
+
+@end
---
+++ WeChatBot/Headers/wechatbot-prefix-header.h
@@ -0,0 +1,15 @@
+//
+//  wechatbot-prefix-header.h
+//  WeChatBot
+//
+//  Created by wz on 2019/3/28.
+//  Copyright © 2019 wz. All rights reserved.
+//
+
+#ifndef wechatbot_prefix_header_h
+#define wechatbot_prefix_header_h
+
+#import "xctheos.h"
+#import "MicroMessengerAppDelegate.h"
+
+#endif /* wechatbot_prefix_header_h */
